eba3f72232d6c59680e00aea4e259f92984af626
Yurun <admin@yurunsoft.com>
2019-08-14 16:25:50 +0800

    初始化仓库

---
+++ WebSocketServer/HttpController/HandShakeController.php
@@ -0,0 +1,31 @@
+<?php
+namespace ImiApp\WebSocketServer\HttpController;
+
+use Imi\Controller\HttpController;
+use Imi\Server\View\Annotation\View;
+use Imi\Server\Route\Annotation\Route;
+use Imi\Server\Route\Annotation\Action;
+use Imi\Server\Route\Annotation\Controller;
+use Imi\Server\Route\Annotation\WebSocket\WSConfig;
+
+/**
+ * 测试
+ * @Controller
+ * @View(renderType="html")
+ */
+class HandShakeController extends HttpController
+{
+    /**
+     * 
+     * @Action
+     * @Route("/")
+     * @WSConfig(parserClass=\Imi\Server\DataParser\JsonObjectParser::class)
+     * @return void
+     */
+    public function index()
+    {
+        // 握手处理，什么都不做，框架会帮你做好
+        
+    }
+    
+}
---
+++ WebSocketServer/config/config.php
@@ -0,0 +1,63 @@
+<?php
+
+use Imi\Log\LogLevel;
+return [
+    'configs'    =>    [
+    ],
+    // bean扫描目录
+    'beanScan'    =>    [
+        'ImiApp\WebSocketServer\Controller',
+        'ImiApp\WebSocketServer\HttpController',
+    ],
+    'beans'    =>    [
+        'SessionManager'    =>    [
+            'handlerClass'    =>    \Imi\Server\Session\Handler\File::class,
+        ],
+        'SessionFile'    =>    [
+            'savePath'    =>    dirname(__DIR__, 2) . '/.runtime/.session/',
+        ],
+        'SessionConfig'    =>    [
+
+        ],
+        'SessionCookie'    =>    [
+            'lifetime'    =>    86400 * 30,
+        ],
+        'HttpDispatcher'    =>    [
+            'middlewares'    =>    [
+                \ImiApp\WebSocketServer\Middleware\PoweredBy::class,
+                \Imi\Server\Session\Middleware\HttpSessionMiddleware::class,
+                \Imi\Server\Http\Middleware\RouteMiddleware::class,
+            ],
+        ],
+        'HtmlView'    =>    [
+            'templatePath'    =>    dirname(__DIR__) . '/template/',
+            // 支持的模版文件扩展名，优先级按先后顺序
+            'fileSuffixs'        =>    [
+                'tpl',
+                'html',
+                'php'
+            ],
+        ],
+        'WebSocketDispatcher'    =>    [
+            'middlewares'    =>    [
+                \Imi\Server\WebSocket\Middleware\RouteMiddleware::class,
+            ],
+        ],
+        'GroupRedis'    =>    [
+            'redisPool'    =>    'redis',
+        ],
+        'ServerGroup'   =>  [
+            'status'        =>  false,
+        ],
+        'HttpDispatcher'    =>    [
+            'middlewares'    =>    [
+                \Imi\Server\WebSocket\Middleware\HandShakeMiddleware::class,
+                \Imi\Server\Http\Middleware\RouteMiddleware::class,
+            ],
+        ],
+        'ConnectContextRedis'    =>    [
+            'redisPool' =>   'redis',
+            'lockId'    =>   'redis',
+        ],
+    ],
+];
